46bd6afd0ebc202c45b9e80cfad4ac19ff1f0800
Sagnik Mandal <sonalisagnik123@gmail.com>
2019-08-04 18:34:33 +0530

    first commit

---
+++ build/precache-manifest.6decf6b0762d02b1e93e79ba1f408e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64f5e63e2715fe1f9de03cd0216d201c",
+    "url": "/ReactDex/static/media/spinner.64f5e63e.gif"
+  },
+  {
+    "revision": "bf8c3a743c6b72ed2dd3",
+    "url": "/ReactDex/static/js/runtime~main.bf8c3a74.js"
+  },
+  {
+    "revision": "54e674f274eb4358245c",
+    "url": "/ReactDex/static/js/main.54e674f2.chunk.js"
+  },
+  {
+    "revision": "598eb22cabda0b5c71df",
+    "url": "/ReactDex/static/js/1.598eb22c.chunk.js"
+  },
+  {
+    "revision": "54e674f274eb4358245c",
+    "url": "/ReactDex/static/css/main.edfc3d67.chunk.css"
+  },
+  {
+    "revision": "598eb22cabda0b5c71df",
+    "url": "/ReactDex/static/css/1.53fa2613.chunk.css"
+  },
+  {
+    "revision": "1a0999fb2c4afbb759bbafed6a37416e",
+    "url": "/ReactDex/index.html"
+  }
+];
